f00631767d766057199482a6466582ca586c4891
Szymon Papuga <szymek.p43@gmail.com>
2016-04-13 21:12:14 +0200

    Dodane przypadki testowe sprawdzajace poprawnosc interakcji z zaleznoscia

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -9,14 +9,16 @@ import org.junit.Test;
 public class SimilarityFinderTest {
 
 	SimilarityFinder similarityFinder;
+	MockSearcher searcher;
 	
 	@Before
 	public void setUp() {
-		similarityFinder = new SimilarityFinder(new MockSearcher());
+		searcher = new MockSearcher();
+		similarityFinder = new SimilarityFinder(searcher);
 	}
 	
 	@Test
-	public void shouldReturnOneForTwoEmptySequences() {
+	public void shouldReturnOneForEmptySequences() {
 		int[] seq1 = new int[0];
 		int[] seq2 = new int[0];
 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
@@ -25,7 +27,7 @@ public class SimilarityFinderTest {
 	}
 	
 	@Test
-	public void shouldReturnZeroForOneEmptySequence() {
+	public void shouldReturnZeroForEmptyAndSingleElemSequences() {
 		int[] seq1 = new int[0];
 		int[] seq2 = new int[] {1};
 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
@@ -52,30 +54,76 @@ public class SimilarityFinderTest {
 	}
 	
 	@Test
-	public void checkResultForTwoSameMultiElemSequences() {
-		int[] seq1 = new int[] {1,2};
-		int[] seq2 = new int[] {1,2};
+	public void shouldReturnOneForSameMultiElemSequences() {
+		int[] seq1 = new int[] {1,2,3};
+		int[] seq2 = new int[] {2,3,1};
 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 		
 		assertThat(result, equalTo(1.0d));
 	}
 	
 	@Test
-	public void checkResultForTwoHalfSameMultiElemSequences() {
-		int[] seq1 = new int[] {1,2,3,4};
-		int[] seq2 = new int[] {5,4,1,6};
+	public void shouldReturnHalfForMultiElemSequences() {
+		int[] seq1 = new int[] {1};
+		int[] seq2 = new int[] {1,2};
 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 		
-		assertThat(result, equalTo(0.33d));
+		assertThat(result, equalTo(0.5d));
 	}
 	
 	@Test
-	public void checkResultForTwoDiffMultiElemSequences() {
-		int[] seq1 = new int[] {1,2,3};
-		int[] seq2 = new int[] {4,5,6};
+	public void shouldReturnZeroForDiffMultiElemSequences() {
+		int[] seq1 = new int[] {1,2,3,4};
+		int[] seq2 = new int[] {5,6,7,8,9};
 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 		
 		assertThat(result, equalTo(0.0d));
+	}
+	
+	@Test
+	public void shouldNotCallSearchMethod() {
+		int[] seq1 = new int[0];
+		int[] seq2 = new int[] {1,2};
+		similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(searcher.getCounter(), equalTo(0));
+	}
+	
+	@Test
+	public void shouldCallSearchMethodOnce() {
+		int[] seq1 = new int[] {1};
+		int[] seq2 = new int[] {1,2};
+		similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(searcher.getCounter(), equalTo(1));
+	}
+	
+	@Test
+	public void shouldCallSearchMethodThreeTimes() {
+		int[] seq1 = new int[] {1,2,3};
+		int[] seq2 = new int[] {1,2};
+		similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(searcher.getCounter(), equalTo(3));
+	}
+	
+	@Test
+	public void shouldCallSearchMethodWithSpecifiedKeyArg() {
+		int key = 1;
+		int[] seq1 = new int[] {key};
+		int[] seq2 = new int[] {2,3};
+		similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(searcher.getKey(), equalTo(key));
+	}
+	
+	@Test
+	public void shouldCallSearchMethodWithSpecifiedSeqArg() {
+		int[] seq1 = new int[] {1};
+		int[] seq2 = new int[] {1,2};
+		similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		
+		assertThat(searcher.getSeq(), equalTo(seq2));
 	}
 	
 }
